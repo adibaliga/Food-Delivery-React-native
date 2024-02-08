9b112cd0950e3c9e7914a1e3660df85336cfb160
adibaliga <adithya_baliga@yahoo.in>
2024-02-08 21:32:05 +0530

    FlatList

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -15,6 +15,7 @@ const ProductListItem = ({ product }: ProductListItemProps) => {
             "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png",
         }}
         style={styles.image}
+        resizeMode="contain"
       />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
@@ -28,12 +29,12 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     borderRadius: 20,
     padding: 10,
-    overflow: "hidden",
+    flex: 1,
+    maxWidth: "50%",
   },
   image: {
     width: "100%",
     aspectRatio: 1,
-    alignSelf: "center",
   },
   title: {
     fontWeight: "600",
